d0e6f9abfd0dfcf5022bcd6dbc69a8febec5e242
Quentin Garchery <garchery.quentin@gmail.com>
2024-10-20 15:31:13 +0200

    chore: remove useless stuff

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -5,8 +5,6 @@ import {Math} from "@utils/libraries/Math.sol";
 
 contract ERC20 {
     uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor() {
         totalSupply = _calculateAppropriateAmount(block.timestamp);
@@ -15,26 +13,4 @@ contract ERC20 {
     function _calculateAppropriateAmount(uint256 timestamp) internal pure returns(uint256) {
         return Math.divDown(timestamp, 2);
     }
-
-    /* Usual ERC20 stuff */
-
-    function transfer(address _to, uint256 _amount) public returns (bool) {
-        return _transfer(msg.sender, _to, _amount);
-    }
-
-    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool) {
-        allowance[_from][msg.sender] -= _amount;
-        return _transfer(_from, _to, _amount);
-    }
-
-    function approve(address _spender, uint256 _amount) public {
-        allowance[msg.sender][_spender] = _amount;
-    }
-
-    function _transfer(address _from, address _to, uint256 _amount) internal returns (bool) {
-        balanceOf[_from] -= _amount;
-        balanceOf[_to] += _amount;
-        return true;
-    }
-
 }
